b045e1c95c9d742a5ea5a20aedc05ca7dbda9988
Daniel Kütt <danielkytt@hotmail.com>
2019-12-04 20:43:17 +0200

    Add css import which does not work

--- aurelia_project/tasks/process-css.ts
+++ aurelia_project/tasks/process-css.ts
@@ -5,10 +5,12 @@ import * as postcss from 'gulp-postcss';
 import * as autoprefixer from 'autoprefixer';
 import * as cssnano from 'cssnano';
 import * as postcssUrl from 'postcss-url';
+import * as postcssImport from 'postcss-import';
 
 export default function processCSS() {
   return gulp.src(project.cssProcessor.source, {sourcemaps: true, since: gulp.lastRun(processCSS)})
     .pipe(postcss([
+      postcssImport(),
       autoprefixer(),
       postcssUrl({url: 'inline', encodeType: 'base64'}),
       cssnano()
